ffb21f3d7732eeef8c606dab9d6f6c34e959e689
tanching <tanching@outlook.com>
2016-02-27 17:50:55 +0800

    Task class

---
+++ src/Task.java
@@ -0,0 +1,17 @@
+
+public class Task {
+	private String description;
+	
+	public Task(String description) {
+		this.description = description;
+	}
+	
+	public void setDescription(String line) {
+		description = line;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+}
