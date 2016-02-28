ad32646e6539512eb6e1e19a63ddb6ea04113d7a
sugarcontent <chuakianchiu@gmail.com>
2016-02-28 18:49:58 +0800

    added SearchCommand class

---
+++ src/logic/SearchCommand.java
@@ -0,0 +1,28 @@
+package logic;
+
+import java.util.ArrayList;
+
+import storage.Storage;
+
+public class SearchCommand
+{
+	
+	private Storage storage;
+//	private Task task;
+	
+	public SearchCommand() // constructor
+	{
+		
+		
+	}
+	
+	
+	public ArrayList<Task> execute(String keyword)
+	{
+		ArrayList<Task> searchResult = storage.searchTask(keyword);
+		
+		return searchResult;
+		
+	}
+	
+}
