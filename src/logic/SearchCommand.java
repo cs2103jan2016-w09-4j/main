9595f88bc4c50c3e5fb08dd843c99554b4ff822a
sugarcontent <chuakianchiu@gmail.com>
2016-03-02 00:19:34 +0800

    Update SearchCommand.java

--- src/logic/SearchCommand.java
+++ src/logic/SearchCommand.java
@@ -2,21 +2,28 @@ package logic;
 
 import java.util.ArrayList;
 
-import common.Task;
 import storage.Storage;
+import common.Task;
 
-public class SearchCommand {
-    
-    private Storage storage;
-    private Task task;
-    
-    public SearchCommand() {
-        
-    }
-    
-    public ArrayList<Task> execute(String keyword) {
-        ArrayList<Task> searchResult = storage.searchTask(keyword);
-        return searchResult;
-    }
-    
+public class SearchCommand
+{
+	
+	private Storage storage;
+//	private Task task;
+	
+	public SearchCommand() // constructor
+	{
+		
+		
+	}
+	
+	
+	public ArrayList<Task> execute(String keyword)
+	{
+		ArrayList<Task> searchResult = storage.searchTask(keyword);
+		
+		return searchResult;
+		
+	}
+	
 }
