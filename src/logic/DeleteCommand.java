d299d97de0599b66764a4e508148b90c2c8e5936
sugarcontent <chuakianchiu@gmail.com>
2016-03-02 00:15:37 +0800

    Update DeleteCommand.java

--- src/logic/DeleteCommand.java
+++ src/logic/DeleteCommand.java
@@ -1,27 +1,25 @@
-package logic;
-
-import java.util.ArrayList;
-
-import logic.TaskList;
-import common.Task;
 import storage.Storage;
 
-public class DeleteCommand {
-    
-    private Task task;
-    private TaskList taskList;
-    private Storage storage;
-    
-    // constructor
-    public DeleteCommand(Task task) {
-        this.task = task;
-    }
-    
-    public ArrayList<Task> execute(int id) {
-        ArrayList<Task> list = storage.deleteTask(id);
-        taskList.deletedTask(list);
-        
-        return list;
-    }
-    
+
+public class DeleteCommand
+{
+	@SuppressWarnings("unused")
+	private Task task;
+	private Storage storage;
+	
+	
+	// constructor
+	public DeleteCommand()  
+	{
+		
+	}
+	
+	public ArrayList<Task> execute(int id)
+	{
+		ArrayList<Task> list = storage.deleteTask(id);
+		
+		return list;
+	}
+	
+	
 }
