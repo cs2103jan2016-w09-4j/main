38e47972e6314b45268f946c6860bf5ff1ad1c28
sugarcontent <chuakianchiu@gmail.com>
2016-03-02 00:17:00 +0800

    Update EditCommand.java

--- src/logic/EditCommand.java
+++ src/logic/EditCommand.java
@@ -2,22 +2,25 @@ package logic;
 
 import java.util.ArrayList;
 
-import logic.TaskList;
 import common.Task;
 import storage.Storage;
 
-public class EditCommand {
-    
-    private Storage storage;
-    private ArrayList<Task> oldList;    
-    
-    public EditCommand(TaskList taskList) {
-        this.oldList = taskList.getTaskList();
-    }
-    
-    public ArrayList<Task> execute(int id, String newDescription) {
-        oldList = storage.editTask(id, newDescription);
-        return oldList;
-    }
-    
+
+public class EditCommand
+{
+	private Storage storage;
+	private ArrayList<Task> oldList;	
+	
+	public EditCommand()
+	{
+		
+	}
+	
+	public ArrayList<Task> execute(int id, String newDescription)
+	{
+		oldList = storage.editTask(id, newDescription);
+		
+		return oldList;
+	}
+	
 }
