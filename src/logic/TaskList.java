bbf7fc3b620ef5539744b83cad93a2e35edfff4e
sugarcontent <chuakianchiu@gmail.com>
2016-03-02 00:20:04 +0800

    Update TaskList.java

--- src/logic/TaskList.java
+++ src/logic/TaskList.java
@@ -1,5 +1,6 @@
 package logic;
 
+
 import java.util.ArrayList;
 
 import common.Task;
@@ -11,47 +12,60 @@ import storage.Storage;
 // This class get the list of Task from storage and hold it so that Logic class can use it from here.
 // It also contains some parts of the command executions
 
-public class TaskList {
-    
-    private int id;
-    private Storage storage;
-    private ArrayList<Task> list;
-    
-    public TaskList() {
-        this.id = 0;
-        this.list = storage.getMainList();
-    }
-    
-    // add the task into the list, then return the task 
-    public void addTaskIntoList(String description) {
-        Task task = new Task(description);
-        this.list.add(task);
-    }
-    
-    public void deletedTask(ArrayList<Task> list) {
-        if(!checkEmpty(list)){
-            setTaskList(list);  // update the list
-        }
-    }
-    
-    public boolean checkEmpty(ArrayList<Task> list) {
-        ArrayList<Task> thisList = getTaskList();
-        if(thisList != null) {
-            return false;
-        }
-        return true;
-    }
-    
-    public ArrayList<Task> getTaskList() {
-        return this.list;
-    }
-    
-    public void setTaskList(ArrayList<Task> list) {
-        this.list = list;
-    }
-    
-    public void exit() {
-        storage.save();
-        // is that all...?
-    }
+public class TaskList
+{
+	
+	@SuppressWarnings("unused")
+	private int id;
+	private Storage storage;
+	private ArrayList<Task> list;
+	
+	
+	public TaskList()
+	{
+		this.id = 0;
+		this.list = storage.getMainList();
+	}
+	
+	// add the task into the list, then return the task 
+	public void addTaskIntoList(String description)
+	{
+		
+		Task task = new Task(description);
+		this.list.add(task);
+		
+	}
+	
+	public void deletedTask(ArrayList<Task> list)
+	{
+		if(!checkEmpty(list)){
+		
+			setTaskList(list);  // update the list
+		}
+		
+		
+	}
+	
+	public boolean checkEmpty(ArrayList<Task> list)
+	{
+		ArrayList<Task> thisList = getTaskList();
+		if(thisList != null)
+		{
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public ArrayList<Task> getTaskList()
+	{
+		return this.list;
+	}
+	
+	public void setTaskList(ArrayList<Task> list)
+	{
+		this.list = list;
+	}
+	
+
 }
