3c7688ebfb93d61c7a53bb4e7ffcaf359f78651a
sugarcontent <chuakianchiu@gmail.com>
2016-03-02 19:19:12 +0800

    Modifying parameters in DeleteCommand

--- src/common/DeleteCommand.java
+++ src/common/DeleteCommand.java
@@ -7,14 +7,13 @@ import storage.Storage;
 public class DeleteCommand {
 
     private Task task;
-	private Storage storage = new Storage();
 	
 	// constructor
 	public DeleteCommand() {
 		
 	}
 	
-	public ArrayList<Task> execute(int id) {
+	public ArrayList<Task> execute(int id, Storage storage) {
 		ArrayList<Task> list = storage.deleteTask(id);
 		return list;
 	}
