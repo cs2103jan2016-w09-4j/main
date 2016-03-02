036204b144260669e711dd34e16111815d52eab2
sugarcontent <chuakianchiu@gmail.com>
2016-03-02 19:17:21 +0800

    Create a Storage instance in logic
    Pass this instance into the Command class, for the commands to use

--- src/logic/Logic.java
+++ src/logic/Logic.java
@@ -1,6 +1,6 @@
 package logic;
 
-import common.Command;
+import storage.Storage;
 import common.Result;
 import parser.Parser;
 
@@ -10,24 +10,23 @@ import parser.Parser;
 // **all the switch cases should be in the parser class, then the the parser class can call the commands from logic class**
 
 public class Logic{
-
+	
 	// Objects to call into other classes
 	private Parser parser;
-	
-	public static void main(String[] args) {
-		Logic logic = new Logic();
-	}
+	private Storage storage = new Storage();
 
 	public Logic(){
+
 		this.parser = new Parser();
 	}
 	
-	public Result processCommand(String input) {
-	    // parse input and retrieve the corresponding command class
-		Command command = parser.parseCommand(input);
-		// execute the command and return the result
-		Result result = command.execute(command);
-		return result;						
+	public Result processCommand(String input)
+	{
+		
+		Command command = parser.parseCommand(input);    // parse the input, and returns the correct command class to go into
+		Result result = command.execute(command, storage);  // goes into whatever command it is and execute and return a result										// after getting back the command then how? give it to command class to allocate to the 
+		return result;						// correct class inside the Command Class?
+		
 	}
 	
 }
