a4fab9a274d68233b9a43d1becdbc66839dfb617
sugarcontent <chuakianchiu@gmail.com>
2016-03-02 19:20:03 +0800

    Modifying parameters in EditCommand

--- src/common/EditCommand.java
+++ src/common/EditCommand.java
@@ -7,14 +7,13 @@ import storage.Storage;
 
 public class EditCommand {
     
-	private Storage storage = new Storage();
 	private ArrayList<Task> oldList;	
 	
 	public EditCommand() {
 		
 	}
 	
-	public ArrayList<Task> execute(int id, String newDescription) {
+	public ArrayList<Task> execute(int id, String newDescription, Storage storage) {
 		oldList = storage.editTask(id, newDescription);
 		return oldList;
 	}
