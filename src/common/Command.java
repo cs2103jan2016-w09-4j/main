acab0a32f17e88b0d9aa2dee35d433cb570600c3
qroll <quek.ruoling@gmail.com>
2016-03-03 16:52:12 +0800

    Fix merging mistake

--- src/common/Command.java
+++ src/common/Command.java
@@ -1,61 +1,13 @@
 package common;
 
 import java.util.ArrayList;
-<<<<<<< HEAD
-
-import common.Result;
-import common.Task;
-=======
->>>>>>> try_stuff
 import storage.Storage;
 
 public class Command {
     public Command(){
     }
 
-<<<<<<< HEAD
-    public Result execute(Command command, Storage storage) {
-        // TODO: this is just a stub to return a usable result
-    	
-    	int commandType = command.getType();
-    	int commandID = command.getId();
-    	String commandDescription = command.getDescription();
-    	ArrayList<Task> list = new ArrayList<Task>();
-    	
-    	// how do i extract out specific info from the Command object?
-    	
-    	switch(commandType){
-    		
-    		case ADD_TYPE:
-    			AddCommand add = new AddCommand();
-    			list = add.execute(commandDescription, storage);
-    			break;
-    			
-    		case DELETE_TYPE:
-    			DeleteCommand delete = new DeleteCommand();
-    			list = delete.execute(commandID, storage);
-    			break;
-    		
-    		case EDIT_TYPE:
-    			EditCommand edit = new EditCommand();
-    			list = edit.execute(commandID, editedDescription, storage);
-    			break;
-    			
-    		case SEARCH_TYPE:
-    			SearchCommand search = new SearchCommand();
-    			list = search.execute(searchWord, storage);
-    			break;
-    			
-    		default:
-    			list.add(new Task("Invalid input!\n"));
-    			return new Result(); // invalid 
-    		}
-    	
-    		Result result = new Result(true, "Success!", list);  // valid
-    		return result;
-=======
-    public ArrayList<Task> execute(Storage storage){
+    public ArrayList<Task> execute(Storage storage) {
         return new ArrayList<Task>();  // dummy
->>>>>>> try_stuff
     }
 }
--- src/logic/Logic.java
+++ src/logic/Logic.java
@@ -6,26 +6,18 @@ import parser.Parser;
 import storage.Storage;
 import java.util.ArrayList;
 
-public class Logic{
+public class Logic {
 
 	// Objects to call into other classes
 	private Parser parser;
 	private Storage storage;
 
-	public Logic(){
+	public Logic() {
 		this.parser = new Parser();
 		this.storage = new Storage();
 	}
-<<<<<<< HEAD
-	
-	public Result processCommand(String input) {
-		Command command = parser.parseCommand(input);    // parse the input, and returns the correct command class to go into
-		Result result = command.execute(command, storage);  // goes into whatever command it is and execute and return a result										// after getting back the command then how? give it to command class to allocate to the 
-		return result;						// correct class inside the Command Class?
-=======
 
-	public Result processCommand(String input)
-	{
+	public Result processCommand(String input) {
 	    System.out.println(input);
 		Command command = parser.parseCommand(input);
 		Result result = new Result(true, "SUCCESS!", command.execute(storage));
@@ -38,7 +30,6 @@ public class Logic{
 		}
 
 		return result;
->>>>>>> try_stuff
 	}
 
 }
