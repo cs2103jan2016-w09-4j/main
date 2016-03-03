7164f8548efa9891a7ac14671306410c8a914bd7
tnkhanh <tnkhanh4@gmail.com>
2016-03-03 14:16:06 +0800

    clearResults

--- src/storage/Storage.java
+++ src/storage/Storage.java
@@ -12,23 +12,23 @@ public class Storage {
 	private static ArrayList<Task> mainList;
 	private static ArrayList<Task> searchResults;
 	private static String fileName;
-	
+
 	private static final String MESSAGE_IOEXCEPTION_ERROR = "IO Exception error";
-	
+
 	// default file name is "mytextfile.txt"
 	public Storage() {
 		mainList = new ArrayList<Task>();
-		searchResults = new ArrayList<Task>();	
+		searchResults = new ArrayList<Task>();
 		fileName = "mytextfile.txt";
 	}
-	
+
 	public Storage(String newFileName) {
 		mainList = new ArrayList<Task>();
 		searchResults = new ArrayList<Task>();
 		fileName = newFileName;
 	}
-	
-	//rewrite whole file 
+
+	//rewrite whole file
 	private void writeToFile() {
 		try {
 			FileWriter writer = new FileWriter(fileName);
@@ -36,39 +36,39 @@ public class Storage {
 				String toWrite = mainList.get(i).getDescription();
 				writer.write(toWrite + "\r\n");
 			}
-			
+
 			writer.close();
 		} catch (IOException ioe) {
 			showUserIOException();
 		}
 	}
-	
+
 	private void appendToFile(Task taskToAdd) {
 		try {
 			FileWriter writer = new FileWriter(fileName,true);
 			BufferedWriter bufferedWriter = new BufferedWriter(writer);
 	        bufferedWriter.write(taskToAdd.getDescription() + "\r\n");
-	        
+
 	        bufferedWriter.close();
 			writer.close();
-			
+
 		} catch (IOException ioe) {
 			showUserIOException();
 		}
 	}
-	
+
 	private static void showUserIOException() {
 		System.out.println(MESSAGE_IOEXCEPTION_ERROR);
 	}
-	
+
 	public ArrayList<Task> addTask(String description) {
 		Task newTask = new Task(description);
 		mainList.add(newTask);
 		appendToFile(newTask);
-		
+
 		return mainList;
 	}
-	
+
 	/*
 	 * This method returns a empty arraylist if the taskID is invalid
 	 * else, it returns the mainList arraylist
@@ -85,7 +85,7 @@ public class Storage {
 		}
 		return mainList;
 	}
-	
+
 	/*
 	 * This method returns a empty arraylist if the taskID is invalid
 	 * else, it returns the mainList arraylist
@@ -103,25 +103,26 @@ public class Storage {
 		}
 		return mainList;
 	}
-	
+
 	/*
 	 * This method returns a empty arraylist if there is no such keyword
-	 * else, it returns the searchResults arraylist 
+	 * else, it returns the searchResults arraylist
 	 */
 	public ArrayList<Task> searchTask(String keyword) {
+	    searchResults.clear();
 		for (int i=0; i<mainList.size(); i++) {
 			if (mainList.get(i).getDescription().contains(keyword)) {
-				searchResults.add(mainList.get(i));				
+				searchResults.add(mainList.get(i));
 			}
 		}
-		
+
 		return searchResults;
 	}
-	
+
 	public void save() {
 	    writeToFile();
 	}
-	
+
 	public ArrayList<Task> getMainList() {
 		return mainList;
 	}
