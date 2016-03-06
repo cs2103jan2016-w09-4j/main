2307e9ce74aaaf40c5a4d7d4ac2fd2134c2350f3
tanching <tanching@Tans-MBP-2.home>
2016-03-07 00:23:24 +0800

    Added save command
    2 methods for save command. one with file name, the other one with
    specific directory and file name

--- src/storage/Storage.java
+++ src/storage/Storage.java
@@ -1,6 +1,7 @@
 package storage;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -14,7 +15,8 @@ public class Storage {
 	private static String fileName;
 
 	private static final String MESSAGE_IOEXCEPTION_ERROR = "IO Exception error";
-
+	private static final String MESSAGE_INVALID_DIRECTORY = "Invalid directory";
+	
 	// default file name is "mytextfile.txt"
 	public Storage() {
 		mainList = new ArrayList<Task>();
@@ -22,11 +24,11 @@ public class Storage {
 		fileName = "mytextfile.txt";
 	}
 
-	public Storage(String newFileName) {
+	/*public Storage(String newFileName) {
 		mainList = new ArrayList<Task>();
 		searchResults = new ArrayList<Task>();
 		fileName = newFileName;
-	}
+	}*/
 
 	//rewrite whole file
 	private void writeToFile() {
@@ -126,9 +128,49 @@ public class Storage {
 
 		return searchResults;
 	}
+	
+	/*
+	 * This method is executed when the user wants to save the 
+	 * data to a specific file, providing the new fileName
+	 */
+	public void saveToFile(String userFileName) {
+		fileName = userFileName;
+		writeToFile();
 
-	public void save() {
-	    writeToFile();
+	}
+	
+	/*
+	 * This method is executed when the user wants to save the 
+	 * data to a specific directory and file, 
+	 * providing the new fileName and directory to save it
+	 * This method will check if the directory is valid
+	 * If the directory is invalid, it will return an error message
+	 * else, the new file with the user's file name will be created in the directory
+	 */
+	public void saveToFileWithDirectory(String directory, String userFileName) {
+		// check if the directory is valid
+		File userDirectory = new File(directory);
+		boolean isValid = userDirectory.isDirectory();
+		if (!isValid) {
+			System.out.println(MESSAGE_INVALID_DIRECTORY);
+		} else {
+			File userDirectoryAndName = new File(directory + "/" + userFileName);
+			writeToFileForSaveCommand(userDirectoryAndName);
+		}
+	}
+
+	private void writeToFileForSaveCommand(File userDirectoryAndName) {
+		try {
+			FileWriter writer = new FileWriter(userDirectoryAndName.getAbsoluteFile());
+			for (int i=0; i<mainList.size(); i++) {
+				String toWrite = mainList.get(i).getDescription();
+				writer.write(toWrite + "\r\n");
+			}
+
+			writer.close();
+		} catch (IOException ioe) {
+			showUserIOException();
+		}
 	}
 
 	public ArrayList<Task> getMainList() {
