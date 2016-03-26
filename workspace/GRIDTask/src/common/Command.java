317dd7012b0f31c919a5531fd7826add286f2601
Gilbert Chua <chuakianchiu@gmail.com>
2016-03-26 23:26:23 +0800

    asdglh

---
+++ workspace/GRIDTask/src/common/Command.java
@@ -0,0 +1,83 @@
+package common;
+
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+
+public class Command {
+
+    public enum CommandType {
+        ADD, DELETE, EDIT, SEARCH, SAVE, LOAD, UNDO, REDO, HOME, DONE, SEARCHDONE, EXIT, INVALID;
+    }
+
+    CommandType type;
+
+    private String description;
+    private int id;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private ArrayList<String> categories;
+
+    public Command(CommandType type) {
+        this.type = type;
+    }
+
+    public Command(CommandType type, String description, LocalDateTime start, LocalDateTime end){ // add
+        this.type = type;
+        this.description = description;
+        this.start = start;
+        this.end = end;
+    }
+
+    public Command(CommandType type, String description) {  // add, search
+        this.type = type;
+        this.description = description;
+    }
+
+    public Command(CommandType type, int id) { // delete
+        this.type = type;
+        this.id = id;
+    }
+
+    public Command(CommandType type, int id, String description) { // edit
+        this.type = type;
+        this.id = id;
+        this.description = description;
+    }
+
+    public Command(CommandType type, int id, String description,  LocalDateTime start, LocalDateTime end) { // edit
+        this.type = type;
+        this.id = id;
+        this.description = description;
+        this.start = start;
+        this.end = end;
+    }
+
+    public Command(String description) {
+        this.description = description;
+    }
+
+    /******************
+     * GETTER METHODS *
+     ******************/
+
+    public CommandType getType() {
+        return type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public LocalDateTime getStartDate(){
+        return start;
+    }
+
+    public LocalDateTime getEndDate(){
+        return end;
+    }
+
+}
---
+++ workspace/GRIDTask/src/ui/DisplayController.java
@@ -0,0 +1,412 @@
+package ui;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+import org.controlsfx.control.HiddenSidesPane;
+
+import common.Command.CommandType;
+import common.Result;
+import common.Task;
+import ui.MainApp;
+import javafx.beans.binding.Bindings;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Side;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Popup;
+import javafx.stage.Stage;
+
+public class DisplayController extends HiddenSidesPane {
+
+    private static Logger logger = Logger.getLogger("MainApp.DisplayController");
+    private MainApp main;
+    private Stage primaryStage;
+    
+    private VBox taskPanel, searchPanel, completedPanel;
+    private SidebarController sidebar;
+    private Popup feedback;
+    
+    private static final String HEADER_COMPLETED_SINGLE = " completed task";
+    private static final String HEADER_COMPLETED_PLURAL = " completed tasks";
+    private static final String HEADER_SEARCH_SINGLE = " search result found";
+    private static final String HEADER_SEARCH_PLURAL = " search results found";
+    
+    private static final String FXML_DISPLAY = "Display.fxml";
+    private static final String RESOURCES_ICON_SUCCESS = "/icons/success-smaller.png";
+    private static final String RESOURCES_ICONS_FAIL = "/icons/fail-smaller.png";
+    
+    private static final int TYPE_COMPLETED = 1;
+    private static final int TYPE_OTHER = 2;
+    private static final int TYPE_TODAY = 3;
+
+    public DisplayController(MainApp main, Stage primaryStage) {
+        this.main = main;
+        this.primaryStage = primaryStage;
+        //initializeLogger();
+        loadFXML();
+        initializeTaskPanel();
+        initializeSearchPanel();
+        initializeCompletedPanel();
+        initializeSidebarContent();
+        initializePopup();
+        handleUserInteractions();
+    }
+
+    private void initializeLogger() {
+        try {
+            Handler fh = new FileHandler("log_ui_display");
+            logger.addHandler(fh);
+            SimpleFormatter formatter = new SimpleFormatter();  
+            fh.setFormatter(formatter);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private void loadFXML() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(FXML_DISPLAY));
+        loader.setController(this);
+        loader.setRoot(this);
+        try {
+            loader.load();
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "failed to load Display", e);
+        }
+    }
+    
+    private void initializeTaskPanel() {
+        taskPanel = new VBox();
+        taskPanel.getStyleClass().add("panel-task");
+        this.setContent(taskPanel);
+        
+        ArrayList<Task> allTasks = main.getTasks();
+        updateTaskPanel(allTasks);
+    }
+
+    private void initializeSearchPanel() {
+        searchPanel = new VBox();
+        searchPanel.getStyleClass().add("panel-search");
+    }
+    
+    private void initializeCompletedPanel() {
+        completedPanel = new VBox();
+        completedPanel.getStyleClass().add("panel-task");
+    }
+
+    private void initializeSidebarContent() {
+        sidebar = new SidebarController(main);
+        this.setLeft(sidebar);
+        this.setTriggerDistance(30);
+    }
+    
+    private void initializePopup() {
+        feedback = new Popup();
+        feedback.addEventFilter(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent event) {
+                feedback.hide();
+            }
+        });
+    }
+    
+    private void handleUserInteractions() {
+        DisplayController display = this;
+        display.addEventFilter(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+            
+            public void handle(KeyEvent event) {
+                if (event.getCode()==KeyCode.M && event.isControlDown()) {
+                    logger.log(Level.INFO, "user toggled sidebar");
+                    if (display.getPinnedSide() == Side.LEFT) {
+                        display.setPinnedSide(null);
+                    } else {
+                        display.setPinnedSide(Side.LEFT);
+                    }
+                } else if (event.getCode()==KeyCode.HOME) {
+                    logger.log(Level.INFO, "user pressed home");
+                    display.setContent(taskPanel);
+                }
+            }
+            
+        });
+    }
+    
+    public void displayResult(Result result) {
+        CommandType cmd = result.getCommandType();
+        if (cmd == CommandType.SEARCH) {
+            updateSearchPanel(result.getResults());
+            this.setContent(searchPanel);
+        } else if (cmd == CommandType.SEARCHDONE) {
+            updateCompletedPanel(result.getResults());
+            this.setContent(completedPanel);
+        } else if (cmd == CommandType.DONE) {
+            sidebar.update();
+            updateCompletedPanel(result.getResults());
+            this.setContent(completedPanel);
+        } else {
+            sidebar.update();
+            updateTaskPanel(result.getResults());
+            showFeedback(cmd, result.getMessage(), result.isSuccess());
+            this.setContent(taskPanel);
+        }
+    }
+    
+    private void updateTaskPanel(ArrayList<Task> allTasks) {
+        assert (taskPanel != null);
+        
+        LocalDateTime todayDateTime = LocalDateTime.now();
+        LocalDate todayDate = todayDateTime.toLocalDate();
+        
+        // place tasks in Today or Others section
+        ArrayList<VBox> todayTasks = new ArrayList<VBox>();
+        ArrayList<VBox> otherTasks = new ArrayList<VBox>();
+        for (Task task : allTasks) {
+            if (task.isSameDate(todayDate)) {
+                todayTasks.add(createToday(task, todayDateTime));
+            } else {
+                otherTasks.add(createOther(task, todayDateTime));
+            }
+        }
+        
+        // build today panel
+        Label todayHeader = createHeader("Today");
+        VBox todayContent = new VBox();
+        if (todayTasks.isEmpty()) {
+            Label empty = new Label("No tasks today");
+            todayContent.getChildren().add(empty);
+        } else {
+            ListView<VBox> todayListView = createListView(todayTasks);
+            todayContent.getChildren().add(todayListView);
+        }
+        
+        VBox todayPanel = new VBox();
+        todayPanel.getChildren().addAll(todayHeader, todayContent);
+        
+        // build other panel
+        Label otherHeader = createHeader("Others");
+        VBox otherContent = new VBox();
+        if (otherTasks.isEmpty()) {
+            Label empty = new Label("No tasks today");
+            otherContent.getChildren().add(empty);
+        } else {
+            ListView<VBox> otherListView = createListView(otherTasks);
+            otherContent.getChildren().add(otherListView);
+        }
+        
+        VBox otherPanel = new VBox();
+        otherPanel.getChildren().addAll(otherHeader, otherContent);
+        
+        // add to task panel
+        taskPanel.getChildren().clear();
+        taskPanel.getChildren().addAll(todayPanel, otherPanel);
+    }
+
+    private void updateSearchPanel(ArrayList<Task> results) {
+        assert (searchPanel != null);
+        LocalDateTime todayDate = LocalDateTime.now();
+        
+        Label searchHeader = createHeader(results.size() + (results.size() == 1 ? HEADER_SEARCH_SINGLE : HEADER_SEARCH_PLURAL));
+        ArrayList<VBox> searchTasks = new ArrayList<VBox>();
+        for (Task result : results) {
+            searchTasks.add(createOther(result, todayDate));
+        }
+        ListView<VBox> searchListView = createListView(searchTasks);
+        
+        searchPanel.getChildren().clear();
+        searchPanel.getChildren().addAll(searchHeader, searchListView);
+    }
+    
+    private void updateCompletedPanel(ArrayList<Task> results) {
+        assert (completedPanel != null);
+
+        Label completedHeader = createHeader(results.size() + (results.size() == 1 ? HEADER_COMPLETED_SINGLE : HEADER_COMPLETED_PLURAL));
+        ArrayList<VBox> completedTasks = new ArrayList<VBox>();
+        for (Task result : results) {
+            completedTasks.add(createCompleted(result));
+        }
+        ListView<VBox> completedListView = createListView(completedTasks);
+        
+        completedPanel.getChildren().clear();
+        completedPanel.getChildren().addAll(completedHeader, completedListView);
+    }
+
+    private void showFeedback(CommandType cmd, String msg, boolean isSuccess) {
+        logger.log(Level.INFO, String.format("showing feedback for %1s, %2s", cmd, isSuccess));
+
+        HBox box = createFeedback(cmd, msg, isSuccess);
+        feedback.getContent().clear();
+        feedback.getContent().add(box);
+        double x = primaryStage.getX() + 10;
+        double y = primaryStage.getY() + primaryStage.getHeight();
+        feedback.setX(x);
+        feedback.setY(y);
+        feedback.show(primaryStage);
+    }
+    
+    private HBox createFeedback(CommandType commandType, String msg, boolean isSuccess) {
+        HBox box = new HBox();
+        box.getStylesheets().add(getClass().getResource("feedback.css").toExternalForm()); 
+        Text message = new Text(msg);
+        ImageView icon;
+        
+        if (isSuccess) {
+            box.setId("popup-success");
+            message.setId("popup-success-text");
+            icon = new ImageView(new Image(RESOURCES_ICON_SUCCESS));
+            icon.setId("popup-success-icon");
+        } else {
+            box.setId("popup-fail");
+            message.setId("popup-fail-text");
+            icon = new ImageView(new Image(RESOURCES_ICONS_FAIL));
+            icon.setId("popup-fail-icon");
+        }
+        box.getChildren().addAll(icon, message);
+        return box;
+        
+    }
+    
+    private Label createHeader(String heading) {
+        Label header = new Label(heading);
+        header.getStyleClass().add("header");
+        header.setMinHeight(USE_PREF_SIZE);
+        return header;
+    }
+
+    private ListView<VBox> createListView(ArrayList<VBox> tasks) {
+        ObservableList<VBox> list = FXCollections.observableArrayList(tasks);
+        ListView<VBox> listView = new ListView<VBox>(list);
+        listView.prefHeightProperty().bind(Bindings.size(list).multiply(65));
+        listView.setMaxHeight(primaryStage.getHeight()/2.1);
+        return listView;
+    }
+    
+    private VBox createToday(Task task, LocalDateTime todayDate) {
+        Label desc = new Label(task.getId() + ". " + task.getDescription());
+        HBox details = createTaskDetails(task, TYPE_TODAY);
+        
+        VBox entry = new VBox();
+        // prevent ListView from showing horizontal scrollbar
+        // when the text description is very long
+        entry.setPrefWidth(1);
+        entry.getChildren().addAll(desc, details);
+        if (task.isModified()) {
+            entry.getStyleClass().add("entry-task-modified");
+        } else if (task.isOverdue(todayDate)) {
+            entry.getStyleClass().add("entry-task-overdue");
+        } else {
+            entry.getStyleClass().add("entry-task");
+        }
+        return entry;
+    }
+    
+    private VBox createOther(Task task, LocalDateTime todayDate) {
+        Label desc = new Label(task.getId() + ". " + task.getDescription());
+        HBox details = createTaskDetails(task, TYPE_OTHER);
+
+        VBox entry = new VBox();
+        // prevent ListView from showing horizontal scrollbar
+        // when the text description is very long
+        entry.setPrefWidth(1);
+        entry.getChildren().addAll(desc, details);
+        if (task.isModified()) {
+            entry.getStyleClass().add("entry-task-modified");
+        } else if (task.isOverdue(todayDate)) {
+            entry.getStyleClass().add("entry-task-overdue");
+        } else {
+            entry.getStyleClass().add("entry-task");
+        }
+        return entry;
+    }
+    
+    private VBox createCompleted(Task task) {
+        Label desc = new Label(task.getDescription());
+        HBox details = createTaskDetails(task, TYPE_COMPLETED);
+        
+        VBox entry = new VBox();
+        // prevent ListView from showing horizontal scroll bar
+        // when the text description is very long
+        entry.setPrefWidth(1);
+        entry.getChildren().addAll(desc, details);
+        entry.getStyleClass().add("entry-task");
+        return entry;
+    }
+    
+    private HBox createTaskDetails(Task task, int type) {
+        HBox details = new HBox();
+        
+        // add priority icon
+        
+        // add date details
+        Label startEndDate = null;
+        if (type == TYPE_TODAY) {
+            DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+            
+            if (task.isDeadline()) {
+                String endDate = timeFormatter.format(task.getEndDate());
+                startEndDate = new Label("By " + endDate);
+            } else if (task.isEvent()) {
+                LocalDateTime start = task.getStartDate();
+                LocalDateTime end = task.getEndDate();
+                LocalDate now = LocalDate.now();
+                if (start.toLocalDate().equals(now) && end.toLocalDate().equals(now)) {
+                    String startDate = timeFormatter.format(start);
+                    String endDate = timeFormatter.format(end);
+                    startEndDate = new Label("From " + startDate + " to " + endDate);
+                } else if (start.toLocalDate().equals(now)) {
+                    String startDate = timeFormatter.format(start);
+                    String endDate = dateFormatter.format(end);
+                    startEndDate = new Label("From " + startDate + " to " + endDate);
+                } else if (end.toLocalDate().equals(now)) {
+                    String startDate = dateFormatter.format(start);
+                    String endDate = timeFormatter.format(end);
+                    startEndDate = new Label("From " + startDate + " to " + endDate);
+                }
+            }
+        } else {
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+            if (task.isDeadline()) {
+                String endDate = dateFormatter.format(task.getEndDate());
+                startEndDate = new Label("By " + endDate);
+            } else if (task.isEvent()) {
+                LocalDateTime start = task.getStartDate();
+                LocalDateTime end = task.getEndDate();
+                String startDate = dateFormatter.format(start);
+                String endDate = dateFormatter.format(end);
+                startEndDate = new Label("From " + startDate + " to " + endDate);
+            }
+        }
+
+        if (startEndDate != null) {
+            startEndDate.getStyleClass().add("details");
+            details.getChildren().add(startEndDate);
+        }
+        
+        // add reminder icon and date
+        
+        // add categories
+        
+        return details;
+    }
+    
+}
