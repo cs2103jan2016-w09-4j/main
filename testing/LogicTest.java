ecff57051da890345036f2fad8fba7418c12e434
qroll <quek.ruoling@gmail.com>
2016-03-29 18:17:16 +0800

    Update integration testing
    Add third user session to test undo and redo commands

--- testing/LogicTest.java
+++ testing/LogicTest.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertArrayEquals;
 
+import java.io.File;
 import java.text.ParseException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -9,6 +10,7 @@ import org.junit.Test;
 
 import common.*;
 import common.Command.CommandType;
+import junitx.framework.FileAssert;
 import logic.Logic;
 
 public class LogicTest{
@@ -41,8 +43,10 @@ public class LogicTest{
         assertEquals(Command.CommandType.INVALID, overdueEnd.getCommandType());
     }
 
+    // Test a single user session: simple CRUD commands, saving and loading 
     @Test
     public void testUserSessionOne() {
+        loadTasks_WithFileName_Success();
         addTask_AllTaskTypes_AddedInOrder();
         editTask_AllTaskTypes_EditedTheSelectedTask();
         deleteTask_AllTaskTypes_DeletedTheSelectedTask();
@@ -68,87 +72,98 @@ public class LogicTest{
     }
 
     public void editTask_AllTaskTypes_EditedTheSelectedTask() {
-    	mainList = logic.getMainList();
     	logic.processCommand("edit 5 Hello World");
     	mainList = logic.getMainList();
-    	/*
-    	compareList = new ArrayList<Task>();
-        compareList.add(new Task("Meeting", null, LocalDateTime.of(2016, 05, 24, 14, 0), 1));
-        compareList.add(new Task("Meeting", null, LocalDateTime.of(2016, 05, 25, 14, 0), 2));
-        compareList.add(new Task("Meeting", LocalDateTime.of(2016, 05, 24, 12, 0), LocalDateTime.of(2016, 05, 26, 14, 0), 3));
-        compareList.add(new Task("Goodbye", null, null, 4));
-        compareList.add(new Task("Hello", null, null, 5));
-        */
-        compareList.get(4).setDescription("Hello World");
+
+    	compareList.get(4).setDescription("Hello World");
         assertArrayEquals(compareList.toArray(), mainList.toArray());
     }
     
     public void deleteTask_AllTaskTypes_DeletedTheSelectedTask() {
-    	mainList = logic.getMainList();
     	logic.processCommand("delete 5");
     	mainList = logic.getMainList();
-    	
-    	compareList = new ArrayList<Task>();
-        compareList.add(new Task("Meeting", null, LocalDateTime.of(2016, 05, 24, 14, 0), 1));
-        compareList.add(new Task("Meeting", null, LocalDateTime.of(2016, 05, 25, 14, 0), 2));
-        compareList.add(new Task("Meeting", LocalDateTime.of(2016, 05, 24, 12, 0), LocalDateTime.of(2016, 05, 26, 14, 0), 3));
-        compareList.add(new Task("Goodbye", null, null, 4));
-        compareList.add(new Task("Hello World", null, null, 5));
-        
-        compareList.remove(4);
+
+    	compareList.remove(4);
         assertArrayEquals(compareList.toArray(), mainList.toArray());
     }
 
     public void saveTasks_WithFileName_Success() {
-        Result result = logic.processCommand("save logic_testFile1.txt");
+        Result result = logic.processCommand("save logic_output1.txt");
         assertEquals(Command.CommandType.SAVE, result.getCommandType());
         assertEquals(true, result.isSuccess());
+        File expectedFile = new File("logic_expected1.txt");
+        File actualFile = new File("logic_output1.txt");
+        FileAssert.assertEquals(expectedFile, actualFile);
     }
 
     public void loadTasks_WithFileName_Success() {
-        logic.processCommand("load logic_testFile1.txt");
+        Result result = logic.processCommand("load logic_output1.txt");
         mainList = logic.getMainList();
-        
-        compareList = new ArrayList<Task>();
-        compareList.add(new Task("Meeting", null, LocalDateTime.of(2016, 05, 24, 14, 0), 1));
-        compareList.add(new Task("Meeting", null, LocalDateTime.of(2016, 05, 25, 14, 0), 2));
-        compareList.add(new Task("Meeting", LocalDateTime.of(2016, 05, 24, 12, 0), LocalDateTime.of(2016, 05, 26, 14, 0), 3));
-        compareList.add(new Task("Goodbye", null, null, 4));
-        //compareList.add(new Task("Hello World", null, null, 5));
+        assertEquals(Command.CommandType.LOAD, result.getCommandType());
+        assertEquals(true, result.isSuccess());
         assertArrayEquals(compareList.toArray(), mainList.toArray());
     }
     
-    /*
+    // Test a single user session: undo and redo
     @Test
-    public void testOne(){  
-        
-        logic.processCommand("add hello");
-        logic.processCommand("add goodbye");
-        logic.processCommand("add meeting 2pm");
-        logic.processCommand("delete 2");
-        logic.processCommand("save logic_testFile1.txt");
-        mainList = logic.getMainList();
-        compareList = logic.processCommand("load logic_expected1.txt").getResults();
-        assertArrayEquals(mainList.toArray(), compareList.toArray());
-        mainList.clear();
-        compareList.clear();
+    public void testUserSessionThree() {
+        logic.processCommand("load logic_expected3.txt");
+        logic.processCommand("save logic_output3.txt");
+        compareList = new ArrayList<Task>();
+        undo_AfterAdd_ReturnToPreviousState();
+        redo_AfterAdd_ReturnToCurrentState();
+        undo_AfterEdit_ReturnToPreviousState();
+        redo_AfterEdit_ReturnToCurrentState();
+        undo_AfterDelete_ReturnToPreviousState();
+        redo_AfterDelete_ReturnToCurrentState();
+        undo_AfterUnallowedCommand_NoChange();
+        redo_AfterUnallowedCommand_NoChange();
     }
-    
-    @Test
-    public void testTwo(){
-        
-        logic.processCommand("add 123456");
-        logic.processCommand("add take me to the top");
-        logic.processCommand("add hello there");
-        logic.processCommand("add myself; yourself");
-        logic.processCommand("edit 1 used to be numbers here");
+
+    private void undo_AfterAdd_ReturnToPreviousState() {
+        logic.processCommand("add New York Cheesecake");
+        logic.processCommand("add Macademia Nut Cookie");
+        logic.processCommand("add Lollipop");
         logic.processCommand("undo");
-        logic.processCommand("save logic_testFile2.txt");
         mainList = logic.getMainList();
-        compareList = logic.processCommand("load logic_expected2.txt").getResults();
-        assertArrayEquals(mainList.toArray(), compareList.toArray());
-        mainList.clear();
-        compareList.clear();
+        
+        compareList.add(new Task("Macademia Nut Cookie", 1));
+        compareList.add(new Task("New York Cheesecake", 2));
+        assertArrayEquals(compareList.toArray(), mainList.toArray());
     }
-    */    
+
+    private void redo_AfterAdd_ReturnToCurrentState() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    private void undo_AfterEdit_ReturnToPreviousState() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    private void redo_AfterEdit_ReturnToCurrentState() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    private void undo_AfterDelete_ReturnToPreviousState() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    private void redo_AfterDelete_ReturnToCurrentState() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    private void undo_AfterUnallowedCommand_NoChange() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    private void redo_AfterUnallowedCommand_NoChange() {
+        // TODO Auto-generated method stub
+        
+    }
 }
