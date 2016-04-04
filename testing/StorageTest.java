e14a6eb2adf13eb6085ddcfc41cbcaa7e61ace33
tanching <tanching@outlook.com>
2016-04-04 20:46:52 +0800

    add test for writeAutoCompletionData

--- testing/StorageTest.java
+++ testing/StorageTest.java
@@ -21,9 +21,10 @@ public class StorageTest {
 	private Storage testStorage;
 	private ArrayList<Task> testList;
 	private ArrayList<Task> getDataFromFile;
-	private Task taskToAdd;
+	private ArrayList<String> autocompletionList;
 	private String fileName1 = "MyTasks.txt";
 	private String fileName2 = "newFile";
+	private String fileName3 = "AutoCompletion.txt";
 
 	private String outputException = "";
 	String expectedDirectory = "/Users/Documents/testDirectory";
@@ -33,6 +34,7 @@ public class StorageTest {
 		System.out.println("initializing");
 		testStorage = new Storage();
 		testList = testStorage.getMainList();
+		autocompletionList = testStorage.getAutoCompletionList();
 		getDataFromFile = new ArrayList<Task>();
 
 		if (testList.isEmpty()) {
@@ -43,21 +45,58 @@ public class StorageTest {
 			System.out.println("Clear mainlist");
 			testList.clear();
 		}
+		
+		if (autocompletionList .isEmpty()) {
+			System.out.println("autocompletion list is empty");
+		} else {
+			System.out.println("Printing autocompletion list");
+			printString(autocompletionList);
+			System.out.println("Clear autocompletion list");
+			autocompletionList.clear();
+		}
 
 		// Add task into testList
-		for (int i = 1; i < 3; i++) {
-			taskToAdd = new Task(String.valueOf(i));
-			testList.add(taskToAdd);
-		}
+		System.out.println("Adding tasks");
+		// first task have all 3 fields
+		Task task1 = new Task("meeting at night so late");
+		// Task task1 = new Task("watch tv");
+		task1.setStart("22/03/2016 13:00");
+		task1.setEnd("22/03/2016 16:00");
+		ArrayList<String> categoryToAdd = new ArrayList<String>();
+		categoryToAdd.add("work");
+		categoryToAdd.add("school");
+		task1.setCategories(categoryToAdd);
+
+		// have 2 fields
+		Task task2 = new Task("gathering");
+		task2.setEnd("25/03/2016 14:00");
+		
+		Task task3 = new Task("do tutorial");
+		ArrayList<String> categoryToAdd2 = new ArrayList<String>();
+		categoryToAdd2.add("homework");
+		task3.setCategories(categoryToAdd2);
+		
+		// have 1 field
+		Task task4 = new Task("meet friends");
+
+		testList.add(task1);
+		testList.add(task2);
+		testList.add(task3);
+		testList.add(task4);
+		
+		//add string as autocompletion
+		String line1 = "add meeting now";
+		String line2 = "gathering at 4pm";
+		autocompletionList.add(line1);
+		autocompletionList.add(line2);
 
 		System.out.println("Print main list");
 		printResult(testList);
 	}
 
 	@Test
-	public void writeToFile_ContentsOfAnotherFile_CorrectNumOfTasks() throws IOException {
+	public void writeToFile_ContentsOfAnotherFile_CorrectNumOfTasksAndContent() throws IOException {
 		String readLine;
-		Task taskFromFile;
 		int count;
 		testStorage.writeToFile();
 
@@ -65,9 +104,16 @@ public class StorageTest {
 		File defaultTextFile = new File(fileName1);
 		BufferedReader readFile = new BufferedReader(new FileReader(defaultTextFile));
 
+		ArrayList<String> stringList = new ArrayList<String>();
 		while ((readLine = readFile.readLine()) != null) {
-			taskFromFile = new Task(readLine);
-			getDataFromFile.add(taskFromFile);
+			stringList.add(readLine);
+		}
+		
+		try {
+			getDataFromFile = testStorage.convertStringToTask(stringList, 0);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
 		assertEquals(testList.size(), getDataFromFile.size());
@@ -83,6 +129,39 @@ public class StorageTest {
 		}
 		// test content
 		assertEquals(testList.size(), count);
+	}
+	
+	@Test
+	public void writeAutoCompletionData_ContentsOfAnotherFile_CorrectNumOfStringandContent() throws IOException {
+		testStorage.writeAutoCompletionData();
+		
+		String readLine;
+
+		// read file into arraylist
+		File defaultTextFile = new File(fileName3);
+		BufferedReader readFile = new BufferedReader(new FileReader(defaultTextFile));
+
+		ArrayList<String> stringList = new ArrayList<String>();
+		while ((readLine = readFile.readLine()) != null) {
+			stringList.add(readLine);
+		}
+
+		System.out.println("size of autocompletionList is " + autocompletionList.size());
+		System.out.println("size of stringList is " + stringList.size());
+
+		assertEquals(autocompletionList.size(), stringList.size());
+
+		int count = 0;
+		for (int i = 0; i < stringList.size(); i++) {
+			String line1 = stringList.get(i);
+			String line2 = autocompletionList.get(i);
+
+			if (line1.equals(line2)) {
+				count++;
+			}
+		}
+		// test content
+		assertEquals(autocompletionList.size(), count);
 	}
 
 	@Test
@@ -163,6 +242,12 @@ public class StorageTest {
 	private static void printResult(ArrayList<Task> testArray) {
 		for (int i = 0; i < testArray.size(); i++) {
 			System.out.println(testArray.get(i).getDescription());
+		}
+	}
+	
+	private static void printString(ArrayList<String> testArray) {
+		for (int i = 0; i < testArray.size(); i++) {
+			System.out.println(testArray.get(i));
 		}
 	}
 }
