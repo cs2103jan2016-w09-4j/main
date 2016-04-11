897c827ec39135f3d674cd643141f957685ecf85
tanching <tanching@outlook.com>
2016-04-11 19:33:33 +0800

    changes to test in StorageTest

--- testing/StorageTest.java
+++ testing/StorageTest.java
@@ -48,26 +48,24 @@ public class StorageTest {
 		getDataFromFile = new ArrayList<Task>();
 
 		if (mainList.isEmpty()) {
-			System.out.println("main list is empty");
+			System.out.println("Main list is empty");
 		} else {
 			mainList.clear();
 		}
 
 		if (autocompletionList.isEmpty()) {
-			System.out.println("autocompletion list is empty");
+			System.out.println("Autocompletion list is empty");
 		} else {
 			autocompletionList.clear();
 		}
 
 		if (completedList.isEmpty()) {
-			System.out.println("completedList is empty");
+			System.out.println("CompletedList is empty");
 		} else {
 			completedList.clear();
 		}
 
-		// Add task into mainList
-		//System.out.println("Adding tasks");
-
+		// add task into mainList
 		// first task have all 3 fields
 		Task task1 = new Task("meeting at night");
 		task1.setStart("22/03/2016 13:00");
@@ -209,7 +207,6 @@ public class StorageTest {
 
 	}
 
-	@Test
 	public void loadFileWithDirectory_NewFileAndDirectory_LoadNewFile()
 			throws NotDirectoryException, FileNotFoundException, IOException, ParseException {
 		testStorage.loadFileWithDirectory(expectedDirectory, testfile);
@@ -227,7 +224,6 @@ public class StorageTest {
 		FileAssert.assertEquals(defaultTextFile, loadFile);
 	}
 
-	@Test
 	public void saveAndLoadWithDirectory_NewFile_SaveAndLoadFile()
 			throws NotDirectoryException, IOException, ParseException {
 		saveToFileWithDirectory_NewFileAndDirectory_FileSaved();
